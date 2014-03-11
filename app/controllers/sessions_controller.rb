1b0e90ee2c129057b1059440206bc9a199dfc425
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 16:19:50 -0600

    Adding a session value for token.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,14 @@ class SessionsController < ApplicationController
     auth = request.env['omniauth.auth']
     user = User.find_by_provider_and_uid(auth['provider'], auth['uid']) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
+    session[:user_token] = auth['credentials']['token']
+
     redirect_to dashboard_url, notice: 'Signed in!'
   end
 
   def destroy
     session[:user_id] = nil
+    session[:user_token] = nil
     redirect_to root_url, notice: 'Signed out!'
   end
 
