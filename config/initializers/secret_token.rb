99721ebb53f6f56ea8e6dff3fae43b36b576556d
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 11:00:12 -0600

    Initial commit. Rails app bootstrap.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Prdashboard::Application.config.secret_key_base = '80b6bb6652c4801ad3a05ae6a64307aaea968eaf93b49c083a89c2cb0e2a79167adbc2c1e8f133de3711a181e173f33556a0c64f9422749568c9f415ffa6b9a6'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Prdashboard::Application.config.session_store :cookie_store, key: '_prdashboard_session'
