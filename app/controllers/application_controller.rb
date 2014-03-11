feb1b408d1e624435e8df896657d0e06810bf44c
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 13:23:51 -0600

    Restricting access to dashboard controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,4 @@
 class ApplicationController < ActionController::Base
-  # Prevent CSRF attacks by raising an exception.
-  # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
   helper_method :current_user
 
@@ -8,6 +6,14 @@ class ApplicationController < ActionController::Base
 
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
+  end
+
+  def authenticate_user!
+    if session[:user_id]
+      current_user
+    else
+      redirect_to root_path unless session[:user_id]
+    end
   end
 
 end
