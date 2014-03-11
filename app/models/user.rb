b7bed13f0d4d9337cfbd6635f3b2fbbd019a04c3
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 15:49:06 -0600

    Adding nickname and avatar columns to Users.

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,8 +2,11 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
-      user.uid = auth['uid']
-      user.name = auth['info']['name']
+      user.uid      = auth['uid']
+      user.name     = auth['info']['name']
+      user.email    = auth['info']['email']
+      user.nickname = auth['info']['nickname']
+      user.avatar   = auth['info']['image']
     end
   end
 
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140311182408) do
+ActiveRecord::Schema.define(version: 20140311215540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,8 @@ ActiveRecord::Schema.define(version: 20140311182408) do
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "nickname"
+    t.string   "avatar"
   end
 
 end
