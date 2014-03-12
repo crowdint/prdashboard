cc9fe87f06c734bb2a6965f9cb23088c1763e735
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 19:53:57 -0600

    Adding scope to github auth.

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,4 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET'], scope: 'user,repo,read:org'
 end
+
