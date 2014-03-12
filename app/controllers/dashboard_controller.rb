8cc847d51083053ac1b186d707fd2db4facb35f6
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 21:12:31 -0600

    Preparing dashboard controller for loading Ember app

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,21 +2,7 @@ class DashboardController < ApplicationController
   layout 'dashboard'
   before_filter :authenticate_user!
 
-  def index
-    @pull_requests = get_pull_requests
-
-    render 'dashboard/index'
-  end
-
-  private
-
-  def get_pull_requests
-    GithubService.new(session[:user_token]).get_pull_requests organization
-  end
-
-  def organization
-    params[:organization] || 'crowdint'
-  end
+  def index; end
 
 end
 
