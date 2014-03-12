bb29987c37fae8509de441771fd01146064ee625
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 19:59:11 -0600

    Adding controller to render the dashboard index HTML or JSON formats.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,5 +1,21 @@
 class DashboardController < ApplicationController
   before_filter :authenticate_user!
 
-  def index; end
+  def index
+    @pull_requests = get_pull_requests
+
+    render 'dashboard/index'
+  end
+
+  private
+
+  def get_pull_requests
+    GithubService.new(session[:user_token]).get_pull_requests organization
+  end
+
+  def organization
+    params[:organization] || 'crowdint'
+  end
+
 end
+
