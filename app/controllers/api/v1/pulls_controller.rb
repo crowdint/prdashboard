9536490701a5fa5d5b4378d5813f1afda527e945
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-11 21:11:40 -0600

    Adding an endpoint for ember.js

---
+++ app/controllers/api/v1/pulls_controller.rb
@@ -0,0 +1,22 @@
+class Api::V1::PullsController < ApplicationController
+
+  def index
+    @pull_requests = get_pull_requests
+    @repositories  = @pull_requests.map(&:repository).uniq { |e| [e.id] }
+    @users         = @pull_requests.map(&:user).uniq { |e| [e.id] }
+
+    render 'api/v1/pulls/index'
+  end
+
+  private
+
+  def get_pull_requests
+    GithubService.new(session[:user_token]).get_pull_requests organization
+  end
+
+  def organization
+    params[:organization] || 'crowdint'
+  end
+
+end
+
