558d320ac21eedcaf28aa56d24328d78e3f563b7
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-12 23:53:47 -0600

    Adding endpoint to retrieve a diff.

---
+++ app/controllers/api/v1/diffs_controller.rb
@@ -0,0 +1,15 @@
+class Api::V1::DiffsController < ApplicationController
+  before_filter :authenticate_user!
+
+  def show
+    render text: GithubService.new(session[:user_token]).get_diff(diff_params)
+  end
+
+  private
+
+  def diff_params
+    params.permit(:id, :repo)
+  end
+
+end
+
