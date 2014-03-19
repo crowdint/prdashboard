a5871feec36588133e2e459f1caf9d1753ca1ef4
Eduardo Figarola <eduardo.figarola@crowdint.com>
2014-03-19 16:25:05 -0600

    Adding Api::V1::DiffsController specs
    Signed-off-by: Edmundo Perez <edmundo.perez@crowdint.com>

---
+++ spec/controllers/api/v1/diffs_controller_spec.rb
@@ -0,0 +1,34 @@
+require 'spec_helper'
+
+describe Api::V1::DiffsController do
+  render_views
+
+  describe '#show' do
+    context 'when user is signed in' do
+      let(:diff) { "le diff" }
+
+      before do
+        GithubService.any_instance.stub(:get_diff).and_return diff
+        signin_user
+        xhr :get, :show, id: 1
+      end
+
+      it 'returns status code 200' do
+        expect(response.status).to eql 200
+      end
+
+      it 'renders the correct text' do
+        expect(response.body).to match /le diff/
+      end
+    end
+
+    context 'when user is not signed in' do
+      it 'returns "invalid credentials" as json message' do
+        xhr :get, :show, id: 1
+
+        expect(response.body).to match /Invalid credentials/
+      end
+    end
+  end
+end
+
